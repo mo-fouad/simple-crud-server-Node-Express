bf59db6e1595fd201b0620b0f501e1d0605ec203
mfo <mfo@ordbogen.com>
2018-08-02 10:05:02 +0200

    DONE

---
+++ routes/product.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router= express.Router();
+
+const product_controller = require('../controllers/product.controller');
+
+// adding some test route
+router.get('/test', product_controller.test);
+
+router.get('/:id', product_controller.product_details);
+
+router.post('/create', product_controller.product_create);
+router.put('/:id/update', product_controller.product_update);
+router.delete('/:id/delete', product_controller.product_delete);
+
+
+module.exports = router;
